3e08e5df6a449c59fcafacf45bbf22fb98393259
Juliana Galkina <galkinaju@rambler.ru>
2024-01-29 18:13:12 +0300

    lesson17 practiceA

---
+++ lesson_17/practiceA.js
@@ -0,0 +1,17 @@
+const a = 21
+console.log(typeof a)
+
+let b = 21578909876578909876n
+console.log(typeof b)
+
+const name = 'Juliana'
+console.log(typeof name)
+
+const qus = true
+console.log(typeof qus)
+
+let und
+console.log(typeof und)
+
+let ob = null
+console.log(typeof ob)
