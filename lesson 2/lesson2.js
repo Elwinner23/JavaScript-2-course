1e3d8e1e82dc191b57b62be63329acb7c6b2b215
= <brazhnik-03@list.ru>
2024-01-29 21:20:25 +0700

    feat: lesson2

---
+++ lesson 2/lesson2.js
@@ -0,0 +1,8 @@
+let name = "Kakich"
+console.log(typeof name)
+let number = 15
+console.log(typeof number)
+let boolean = true
+console.log(typeof boolean)
+let bugint = 555n
+console.log(typeof bugint)
