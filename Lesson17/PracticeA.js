9129c8515760e225f12b41c554e2d1ea33d5ad71
“Valery” <117990439+ValeryManoilov@users.noreply.github.com>
2024-01-29 17:03:20 +0300

    Complete Lesson17 PracticeA

---
+++ Lesson17/PracticeA.js
@@ -0,0 +1,10 @@
+let name
+console.log(typeof name)
+name = null
+console.log(typeof name)
+name = "Валерий"
+console.log(typeof name)
+name = 503
+console.log(typeof name)
+name = true
+console.log(typeof name)
