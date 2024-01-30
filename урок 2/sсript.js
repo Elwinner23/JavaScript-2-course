cb96ffb8b88c78c598c7f576db35715a86cc239d
= <=>
2024-01-30 19:43:47 +0500

    fork

---
+++ урок 2/sсript.js
@@ -0,0 +1,10 @@
+let s = "stroka"
+console.log(typeof s)
+let n = 21
+console.log(typeof n)
+let b = true
+console.log(typeof b)
+let u = null
+console.log(typeof u)
+let x = 3721n
+console.log(typeof x)
