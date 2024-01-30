9d0331a048f03f1b4a4a2c03c99116ba2f491b2f
John Doe <johndoe@example.com>
2024-01-30 17:18:44 +0300

    lesson 17

---
+++ lesson16/lesson17.js
@@ -0,0 +1,15 @@
+let a ="есть хочу"
+console.log(typeof a);
+let b = 12
+console.log(typeof b);
+let c = false
+console.log(typeof c);
+let d = null
+console.log(typeof d);
+let x = 12345n
+console.log(typeof x);
+
+console.log(typeof e);
+const e = 11;
+
+
