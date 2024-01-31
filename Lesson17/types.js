b15c6a99b5cd3314fbb4a11a12e659aff10e0dd7
Ivanova Daria <ida30062006@gmail.com>
2024-01-31 22:16:32 +0300

    lesson17, A (the whole B was done in les16)

---
+++ Lesson17/types.js
@@ -0,0 +1,8 @@
+console.log(typeof 13)
+console.log(typeof "13")
+console.log(typeof true)
+console.log(typeof 13n)
+console.log(typeof null)
+console.log(typeof n)
+let id = Symbol("s");
+console.log(typeof id)
