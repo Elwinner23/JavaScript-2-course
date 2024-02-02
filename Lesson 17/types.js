5742aed9a18a0b6f873b3e82d9b0b20faac2c015
Юрий <zaretskiy.y.a@gmail.com>
2024-02-02 08:40:56 +0300

    feat lesson 17 B

---
+++ Lesson 17/types.js
@@ -0,0 +1,8 @@
+console.log(typeof 1)
+console.log(typeof "1")
+console.log(typeof true)
+console.log(typeof 1n)
+console.log(typeof null)
+console.log(typeof n)
+let id = Symbol("y");
+console.log(typeof id)
