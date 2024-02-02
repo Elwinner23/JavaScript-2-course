0077a6d4fe74481ab9fe0412d7854ade0d898d56
Ivanova Daria <ida30062006@gmail.com>
2024-02-02 21:57:42 +0300

    les18 pA

---
+++ Lesson18/calc.js
@@ -0,0 +1,26 @@
+var i = "do"
+while (i){
+  	i = prompt("Введите первое число или введите ex", "");
+  	if (i === "ex") {
+    	break;
+  	}
+  	var m = prompt("Введите второе число", "");
+  
+  	var op = prompt("Введите математический оператор", "");
+    var result
+    switch (op) {
+        case '+':
+          result = +i + +m;
+          break;
+        case '-':
+          result = +i - +m;
+          break;
+        case '*':
+          result = +i * +m;
+          break;
+        case '/':
+          result = +i / +m;
+          break;
+      }
+    alert(result)
+}
