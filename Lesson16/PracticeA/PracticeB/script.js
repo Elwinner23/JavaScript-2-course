eb55b00673f3a1f6ad860cd01a9a9614d19d1976
timurkaff <timalololohka@gmail.com>
2024-02-02 17:41:04 +0400

    feat:(Сдлелал Practice B)

---
+++ Lesson16/PracticeA/PracticeB/script.js
@@ -0,0 +1,7 @@
+window.loginform = function() {
+    var name = document.getElementById('fullName').value;
+    var email = document.getElementById('email').value;
+
+    console.log("Имя: " + name);
+    console.log("почта: " +  email);
+}
