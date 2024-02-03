dd06267c186b8e91719ae0d110e6fadf353399f5
timurkaff <timalololohka@gmail.com>
2024-02-03 11:01:47 +0400

    feat:(Сделал Lesson 16-17)

---
+++ Lesson17/PracticeA/script.js
@@ -0,0 +1,12 @@
+let name = "Timur";
+let age = 12;
+let bool = true;
+
+console.log("Моё имя: " + name);
+console.log("Мне: " + age);
+console.log(`Я участник КОД ?: ${bool ? "да" : "нет"}`);
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof bool);
