c51158a4dabcccbf1a999dd4ac766aff44990975
Timurkaff <timalololohka@gmail.com>
2024-02-03 22:48:10 +0400

    feat:(Сделал Practice A)

---
+++ Урок 18/PracticeA/script.js
@@ -0,0 +1,28 @@
+var op;
+
+function calculator() {
+  var result;
+
+  var num1 = Number(document.getElementById("num1").value);
+  var num2 = Number(document.getElementById("num2").value);
+
+  switch(op){
+    case '+':
+      result = num1 + num2;
+        break;
+    
+    case '-':
+      result = num1 - num2;
+        break;
+
+    case '*':
+      result = num1 * num2;
+        break;
+    
+    case "/":
+      result = num1 / num2;
+        break;
+}
+
+document.getElementById('result').innerHTML = result;
+}
