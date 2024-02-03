9e32c7d6a340ac29ecb35689df4c37f7903439b3
= <=>
2024-02-03 17:20:17 +0300

    fix: практика 17 урок

---
+++ Урок 18/messenger/mes.js
@@ -0,0 +1,11 @@
+console.log(prompt('Введите ваше ФИО'));
+console.log(prompt('Введите вашу электронную почту'));
+
+let a = "текст"
+let b = "17"
+let c = "17n"
+let d = "true"
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
