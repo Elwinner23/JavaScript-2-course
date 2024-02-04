c644abad3d4e3014aa64b331075fd119327d073e
= <=>
2024-02-04 13:04:13 +0300

    fix: практика А 18 урок

---
+++ taskA.js
@@ -0,0 +1,54 @@
+let a = 10
+a = -a
+console.log(a)
+
+
+let a = 10, b = 5
+c = a - b
+console.log(c)
+
+
+let a = 10, b = 5
+c = a + b
+console.log(c)
+
+
+let a = 10
+console.log(a % 5)
+
+
+let a = 10
+console.log(a ** 5)
+
+
+let a = "kod"
+let a = "pazl"
+console.log(a + b)
+console.log(b + a)
+
+
+let a = 10
+let b = "5"
+console.log(a + b)
+console.log(b + a)
+console.log(a - b)
+console.log(b - a)
+console.log(a + +b)
+console.log(a + Number(b))
+console.log(a + 7)
+console.log(String(a) + b)
+
+
+let a = 10
+a += 5
+console.log(a)
+
+
+let a = 10
+a ++
+console.log(a)
+
+
+let a = 10
+a --
+console.log(a)
