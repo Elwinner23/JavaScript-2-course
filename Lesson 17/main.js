1507a0fb0245eb2775a58ef4b0d12ef487907359
MisterCat100 <mistercat0618@gmail.com>
2024-02-05 20:31:14 +0300

    feat(Lesson17): completed Practices A-B-C.

---
+++ Lesson 17/main.js
@@ -0,0 +1,8 @@
+console.log(typeof 1)
+console.log(typeof 12312n)
+console.log(typeof "dasdasd")
+console.log(typeof true)
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof Object)
