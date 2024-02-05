3d5fe40d31a99ec854eee1c5a30e8b3e62f1dfa4
= <brazhnik-03@list.ru>
2024-02-05 20:10:09 +0700

    feat: lesson 2

---
+++ Урок 18/taskA.js
@@ -0,0 +1,16 @@
+let a = 3
+console.log(a - 2)
+let b = 25
+console.log(b ** 5)
+let c = 125
+console.log(c % 6)
+let d = 'fron'
+console.log(d + 'tend')
+console.log(a + d, d + a)
+let i = '7'
+console.log(c - i)
+console.log(c + +i)
+console.log(c + Number(i))
+console.log(a += 5)
+a++
+console.log(a)
