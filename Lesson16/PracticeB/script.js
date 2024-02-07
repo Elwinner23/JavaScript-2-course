4d1c1bc0c49b747bb637dc46bf09970f27d61021
timurkaff <timalololohka@gmail.com>
2024-02-07 08:46:57 +0300

    feat:(Улшил форму зполненения)

--- Lesson16/PracticeB/script.js
+++ Lesson16/PracticeB/script.js
@@ -1,7 +1,15 @@
 window.loginform = function() {
     var name = document.getElementById('fullName').value;
     var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var papassword = document.getElementById('pa-password').value;
 
     console.log("Имя: " + name);
     console.log("почта: " +  email);
+    console.log("Пароль: " + papassword);
+
+    if(password !== papassword){
+    alert("Пароли не похожы")
+    return;
+    }
 }
