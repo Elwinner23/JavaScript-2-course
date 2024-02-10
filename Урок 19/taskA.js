4bee209cb60097453f00cb11347b5abc7b44ef96
= <=>
2024-02-10 17:15:11 +0300

    fix: исправила практику

--- Урок 19/taskA.js
+++ Урок 19/taskA.js
@@ -16,40 +16,7 @@ switch (b) {
         break;
     case '/':
         alert(a / c);
-}
-
-
-
-let a = prompt('Введите число');
-a = +a;
-let b = Math.floor(Math.random() * 101);
-
-if (a == b) {
-  alert('Ты угадал!');
-}
-else {
-  alert ('Ты не угадал(');
-}
-
-
-
-let a = prompt('Введите число 1');
-a = +a;
-let c = prompt('Введите число 2');
-c = +c;
-let d = prompt('Введите число 3');
-d = +d;
-let b = Math.floor(Math.random() * 101);
-
-if (a == b) {
-  alert('Ты угадал!');
-}
-else if (c == b) {
-  alert('Ты угадал!');
-}
-else if (d == b) {
-  alert('Ты угадал!');
-}
-else {
-  alert ('Ты не угадал(');
+        break;
+    default:
+        alert('Вы ввели что-то неправильно(');
 }
---
+++ Урок 19/taskB.js
@@ -0,0 +1,10 @@
+let a = prompt('Введите число');
+a = +a;
+let b = Math.floor(Math.random() * 101);
+
+if (a == b) {
+  alert('Ты угадал!');
+}
+else {
+  alert ('Ты не угадал(');
+}
