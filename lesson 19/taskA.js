634f3f3073d7c1cedafd0f7a6a5f95ab820343ae
= <serdg.suvorov@mail.ru>
2024-02-10 17:11:35 +0300

    feat: выполнил практику A

---
+++ lesson 19/taskA.js
@@ -0,0 +1,22 @@
+let a = prompt("Введите число");
+a = Number(a);
+let b = prompt("Введите еще 1 число");
+b = Number(b)
+let c = prompt("Введите математический оператор");
+
+switch (c) {
+  case "+" : alert(a+b);
+    break;
+ case "*" : alert(a*b);
+    break;
+ case "-" : alert(a-b);
+    break;
+ case "/" : alert(a/b);
+    break;
+ case "**" : alert(a**b);
+    break;
+ case "%" : alert(a%b);
+    break;
+default: oops, error;
+};
+console.log(`Your result ${c}`);
