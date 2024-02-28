d92ed94e4fafc5fbdf407610b7b6b43371da2419
Juliana Galkina <galkinaju@rambler.ru>
2024-02-28 16:37:49 +0300

    21les and A25

--- Lesson 21/practiceC.js
+++ Lesson 21/practiceC.js
@@ -1,46 +1,58 @@
 class Users{
-    #name;
-    #id;
-    #role;
-    #password;
-    
-    get name(){
-      return this.#name;
-    }
-    get id(){
-      return this.#id;
-    }
-    
-    get password(){
-      return this.#password;
-    }
-    
-    get role(){
-      return this.#role;
-    }
-    
-     set password(newPassword){
-       if (this.#role == 'admin'){
-         this.#password = newPassword;
-         console.log('Вы поменяли пароль')
-       }
-       else{
-         console.log('Вы не admin')
-       }
-     }
-    
-    
-    constructor(name, id,  role, password){
-      this.#name = name
-      this.#id = id
-      this.#role = role
-      this.#password = password
-    }
+  #name;
+  #id;
+  #role;
+  #password;
+  
+  get name(){
+    return this.#name;
+  }
+  get id(){
+    return this.#id;
   }
   
-  let user1 = new Users('Juliana', Date.now(), 'admin', 'qwerty123')
-  console.log(user1.password)
-  user1.password = 'qwerty123JG'
-  console.log(user1.password)
+  get password(){
+    return this.#password;
+  }
   
+  get role(){
+    return this.#role;
+  }
+  set name(newName){
+    this.#name = newName
+  }
   
+  ChangeP(){
+    let lastP = prompt('Введите текущий пароль:')
+    if (lastP == this.#password){
+      let newP1 = prompt('Введите новый пароль:')
+      if (newP1 != this.#password){
+        let newP2 = prompt('Повторите новый пароль:')
+          if (newP1 == newP2){
+            alert('Пароль успешно изменён')
+              this.#password = newP1
+          }
+           else{alert('Пароли должны совпадать')}
+    }
+       else{
+        alert('Старый и новый пароль должны различаться')
+      }
+  }
+     else{
+       alert('Пароль неверный')}
+  }   
+  
+  constructor(name, id,  role, password){
+    this.#name = name
+    this.#id = id
+    this.#role = role
+    this.#password = password
+  }
+}
+
+let user1 = new Users('Juliana', Date.now(), 'admin', 'qwerty123')
+console.log(user1.password)
+user1.ChangeP()
+console.log(user1.password)
+
+
---
+++ lesson 25/practice A/PracticeA.js
@@ -0,0 +1,3 @@
+const array = [1, -2, 4, 56, -77]
+const filtredar = array.filter(i => i >0)
+console.log(filtredar)
