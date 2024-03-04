32c99b2358b199c3c2180a114797d9a3efa746e0
Ivanova Daria <ida30062006@gmail.com>
2024-03-04 16:43:06 +0300

    feat: жалкие попытки сделать практику B (но я доделаю)

---
+++ Lesson26/dist/card.5f39e005.js
@@ -0,0 +1,20 @@
+let inputNumber = document.getElementById("card-number");
+inputNumber.addEventListener("input", (event)=>{
+    const inputText = event.target.value.replaceAll(" ", "").replace(/\D/g, "");
+    const formattedText = inputText.replace(/(.{4})/g, "$1 ");
+    event.target.value = formattedText.trim();
+});
+let inputDate = document.getElementById("card-date");
+inputDate.addEventListener("input", (event)=>{
+    const inputText = event.target.value.replaceAll(" ", "").replace(/\D/g, "");
+    const formattedText = inputText.replace(/(.{2})/g, "$1/");
+    event.target.value = formattedText.trim();
+});
+let inputCVV = document.getElementById("cvv-code");
+inputCVV.addEventListener("input", (event)=>{
+    const inputText = event.target.value.replaceAll(" ", "").replace(/\D/g, "");
+    const formattedText = inputText.replace(/(.{5})/g, "$1/");
+    event.target.value = formattedText.trim();
+});
+
+//# sourceMappingURL=card.5f39e005.js.map
