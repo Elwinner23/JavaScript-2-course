d4b12ee79990c705bc2140aff9b12dfed58d4d92
= <=>
2024-03-05 17:02:49 +0300

    fix: я пыталась, но локал хост не работает

--- Урок 26/tasks.js
+++ Урок 26/tasks.js
@@ -1,37 +1,37 @@
 import cardValidator from "card-validator";
 
 function pay() {
-   const number_card = document.getElementById('card-number').value;
+ const number_card = document.getElementById('card-number').value;
 
-   const validationResult = cardValidator.number(number_card);
+ const validationResult = cardValidator.number(number_card);
 
-   console.log(validationResult);
+ console.log(validationResult);
 
-   if (validationResult.isValid) {
-      console.log('Номер карты валиден');
-   } else {
-      alert('Номер карты не валиден')
-   }
+ if (validationResult.isValid) {
+    console.log('That is right');
+ } else {
+    alert('The card number is incorrect')
+ }
 }
 
 function input_card(input) {
-   const cardNumber = input.value.replace(/\D/g,'');
-   let formatcardnumber = '';
-   for (var i = 0; i < cardNumber.length; i += 4) {
-      if (i !== 0) {
+ const cardNumber = input.value.replace(/\D/g, '');
+ let formatcardnumber = '';
+ for (var i = 0; i < cardNumber.length; i += 4) {
+    if (i !== 0) {
       formatcardnumber += '-';
-      }
-      formatcardnumber += cardNumber.substring(i, i + 4);
-   }
-   return formatcardnumber;
+    }
+    formatcardnumber += cardNumber.substring(i, i + 4);
+ }
+ return formatcardnumber;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-   const button = document.getElementById('card-btn');
-   button.addEventListener('click', pay);
+ const button = document.getElementById('card-btn');
+ button.addEventListener('click', pay);
 
-   const cardNumberInput = document.getElementById('card-number');
-   cardNumberInput.addEventListener('input', function() {
-      this.value = input_card(this);
-   });
+ const cardNumberInput = document.getElementById('card-number');
+ cardNumberInput.addEventListener('input', function() {
+    this.value = input_card(this);
+ });
 });
