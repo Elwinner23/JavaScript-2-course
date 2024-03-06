8ffd01b0e345c653ba60cdf4fdec8d52ef40839d
timurkaff <timalololohka@gmail.com>
2024-03-06 19:41:24 +0400

    fix

--- Lesson 26/dist/index.672d4772.js
+++
@@ -1,8 +0,0 @@
-function validateCardNumber(cardNumber) {
-    const validationResult = cardValidator.number(cardNumber);
-    if (validationResult.isValid) console.log("Номер карты валиден");
-    else console.log("Номер карты невалиден");
-}
-validateCardNumber("4111111111111111");
-
-//# sourceMappingURL=index.672d4772.js.map
