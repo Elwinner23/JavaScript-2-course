66d9944496426cc1957de0663723194a36be0ad4
Ivanova Daria <ida30062006@gmail.com>
2024-03-07 14:16:27 +0300

    fix: удачная попытка сделать les27 prB

--- Lesson27/practiceB/front/script.js
+++ Lesson27/practiceB/front/script.js
@@ -1,8 +1,15 @@
-
-
-let button = document.getElementById('ident')
-
-fetch('http://localhost:3000')
-    .then((response) => {
-        console.log(response.status)
-    } )
+let button = document.getElementById('ident');
+button.addEventListener('click', (event) =>{
+    fetch('http://localhost:3000/')
+  .then(response => {
+    console.log(response.status); // Выведет статус код ответа
+    if (response.ok) {
+        button.style.backgroundColor = 'green';
+    } else {
+        button.style.backgroundColor = 'red';
+    }
+  })
+  .catch(error => {
+    console.error('Ошибка:', error);
+  });
+})
--- Lesson27/practiceB/index.js
+++ Lesson27/practiceB/index.js
@@ -1,7 +1,4 @@
-import cors from "C:\Users\Daria\OneDrive\Рабочий стол\kod\JavaScript-2-course\Lesson27\practiceB\node_modules\cors\lib\index.js"
-const http = require('http');
-
-
+const http = require('http')
 // Функция для генерации случайного статус-кода
 function getRandomStatusCode() {
    const statusCodes = [200, 201, 400, 401, 404, 500]; // Пример возможных статус-кодов
@@ -13,20 +10,21 @@ function getRandomStatusCode() {
 // Создание HTTP-сервера
 const server = http.createServer((req, res) => {
    const statusCode = getRandomStatusCode();
-
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
    // Установка случайного статус-кода
    res.writeHead(statusCode, {'Content-Type': 'text/plain'});
 
 
    // Отправка ответа
-   res.end("Response with status code: ${statusCode}\n");
-});
+   res.end(`Response with status code: ${statusCode}\n`);
+})
 
- server.use(cors())
 const port = 3000;
 // Слушаем порт 3000
 server.listen(port, () => {
-   console.log("Server running at http://localhost:${port}/");
-});
-
+   console.log(`Server running at http://localhost:${port}/`);
+})
