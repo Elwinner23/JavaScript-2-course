9bfe50d936c2e780640e2a8982513d26268fd16c
Grixa08 <brazhnik-03@list.ru>
2024-03-10 23:03:02 +0700

    feat; Практика А, останьные не знаю

---
+++ Урок 28/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function() {
+    let loginField = document.querySelector('#login');
+    let passwordField = document.querySelector('#password');
+    let checkbox = document.querySelector('#checkbox');
+    let button = document.querySelector('#loginButton');
+    let button1 = document.querySelector('#exit');
+  
+    //события на кнопке
+    button.addEventListener('click', function() {
+      // заполнение полей
+      if (loginField.value && passwordField.value) {
+        // состояние чекбокса
+        let isChecked = checkbox.checked;
+  
+        // Сохраняем состояние чекбокса в localstorage
+        localStorage.setItem('isChecked', isChecked);
+  
+        // Переходим на страницу main.html
+        window.location.replace('main.html');
+      }
+    });
+  
+    // Проверяем, есть ли данные в localstorage
+    let isChecked = localStorage.getItem('isChecked');
+  
+    // Если есть данные и состояние чекбокса было отмечено, то ставим галочку
+    if (isChecked === 'true') {
+      checkbox.checked = true;
+    }
+  
+    // Проверяем, находимся ли мы на странице main.html
+    // Если да, то делаем проверку
+    if (window.location.href.includes('main.html') && !isChecked) {
+      window.location.replace('authorization.html');
+    }
+  });
