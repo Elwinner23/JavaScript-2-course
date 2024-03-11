8226ce95ea93dc1c6fd56324d3801e64fda4882b
John Doe <johndoe@example.com>
2024-03-11 09:34:02 +0300

    ваааааааааааааааааааа

--- lesson 28/main.js
+++ lesson 28/main.js
@@ -1,3 +1,8 @@
 window.addEventListener('DOMContentLoaded', function(){
     let checkbox = document.querySelector('.checkbox');
-})
+    checkbox.checked = localStorage.getItem('isChecked') === 'true'; // восстановим состояние чекбокса из LocalStorage
+    checkbox.addEventListener('click', function(){
+        localStorage.setItem('isChecked', checkbox.checked ? 'true' : 'false'); // сохраняем состояние чекбокса в LocalStorage
+    });
+});
+
