255c96a442df06acd789a9bd7b262d4f21a02632
Ivanova Daria <ida30062006@gmail.com>
2024-03-14 00:16:23 +0300

    feat: les29 prB

--- Lesson29/src/validate.js
+++ Lesson29/src/validate.js
@@ -1,5 +1,10 @@
 import { isSecurityCodeValid } from 'creditcard.js';
+import { isValid } from 'creditcard.js';
 
 export default function validateCVV(cardNumber, CVVNumber) {
   return isSecurityCodeValid(cardNumber, CVVNumber) === true;
+}
+
+export default function validateCardNumber(cardNumber) {
+  return isCodeValid(cardNumber) === true;
 }
--- Lesson29/src/validate.test.js
+++ Lesson29/src/validate.test.js
@@ -1,4 +1,6 @@
 import  validateCVV  from './validate';
+import validateCardNumber from './validate.js';
+
 
 describe('validateCVV', () => {
   it('Валидация CVV/CVC пропускает строку с тремя цифровыми символами', () => {
@@ -23,5 +25,32 @@ describe('validateCVV', () => {
   it('Валидация CVV/CVC не пропускает строки с тремя и более нецифровыми символами.', () => {
     const validNumber = '4242 4242 4242 4242';
     expect(validateCVV(validNumber, 'abcde')).toBe(false);
+  });
+});
+
+describe('validateCardNumber', () => {
+
+  //1
+  it('Валидация номера карты не пропускает некорректный номер карты', () => {
+    const validNumber = '1234 4242 4242 4242';
+    expect(validateCardNumber(validNumber)).toBe(false);
+  });
+
+  //2
+  it('Валидация номера карты не пропускает произвольную строку, содержащую любые нецифровые символы.', () => {
+    const validNumber = '4242 abcd 4242 4242';
+    expect(validateCVV(validNumber)).toBe(false);
+  });
+
+  //3
+  it('Валидация номера карты не пропускает строку с недостаточным количеством цифр', () => {
+    const validNumber = '4242 4242 4242 42';
+    expect(validateCVV(validNumber)).toBe(false);
+  });
+
+  //4
+  it('Валидация номера карты не пропускает строку со слишком большим количеством цифр.', () => {
+    const validNumber = '4242 4242 4242 4242 2222';
+    expect(validateCVV(validNumber)).toBe(false);
   });
 });
