5530958754d7a64950517982ff9cbe713f8f9fa0
yuraz28 <zaretskiy.y.a@gmail.com>
2024-03-16 00:25:21 +0300

    fix lesson26 ABC

--- Lesson26/src/validate.js
+++
@@ -1,5 +0,0 @@
-import { isSecurityCodeValid } from 'creditcard.js';
-
-export default function validateCVV(cardNumber, CVVNumber) {
-  return isSecurityCodeValid(cardNumber, CVVNumber) === true;
-}
