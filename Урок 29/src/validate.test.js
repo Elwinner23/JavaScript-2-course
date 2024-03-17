ef0a91b251690d1eef3dbfa1db9c857a02a7159a
= <=>
2024-03-17 13:15:52 +0300

    fix: практиув 30 урок

--- Урок 29/src/validate.test.js
+++ Урок 29/src/validate.test.js
@@ -1,56 +1,40 @@
-import  validateCVV  from './validate';
-import validateCardNumber from './validate.js';
-
+import  validateCVV  from './validate.js';
+import validateCardNumber from './validate1.js';
 
 describe('validateCVV', () => {
   it('Валидация CVV/CVC пропускает строку с тремя цифровыми символами', () => {
     const validNumber = '4242 4242 4242 4242';
     expect(validateCVV(validNumber, '123')).toBe(true);
   });
-
-  //1
-  it('Валидация CVV/CVC не пропускает строки с 1-2 цифровыми символами.', () => {
+  it('Валидация CVV/CVC не пропускает строки с 1-2 цифровыми символами', () => {
     const validNumber = '4242 4242 4242 4242';
-    expect(validateCVV(validNumber, '12')).toBe(false);
-    expect(validateCVV(validNumber, '1')).toBe(false);
+    expect(validateCVV(validNumber, '13')).toBe(false);
   });
-
-  //2
-  it('Валидация CVV/CVC не пропускает строки с 4+ цифровыми символами.', () => {
+  it('Валидация CVV/CVC не пропускает строки с 4+ цифровыми символами', () => {
     const validNumber = '4242 4242 4242 4242';
-    expect(validateCVV(validNumber, '12345')).toBe(false);
+    expect(validateCVV(validNumber, '13123')).toBe(false);
   });
-
-  //3
-  it('Валидация CVV/CVC не пропускает строки с тремя и более нецифровыми символами.', () => {
+  it('Валидация CVV/CVC не пропускает строки с тремя и более нецифровыми символами', () => {
     const validNumber = '4242 4242 4242 4242';
-    expect(validateCVV(validNumber, 'abcde')).toBe(false);
+    expect(validateCVV(validNumber, 'asdfas')).toBe(false);
   });
 });
 
 describe('validateCardNumber', () => {
-
-  //1
   it('Валидация номера карты не пропускает некорректный номер карты', () => {
-    const validNumber = '1234 4242 4242 4242';
+    const validNumber = '1233 4242 4242 4242';
     expect(validateCardNumber(validNumber)).toBe(false);
   });
-
-  //2
-  it('Валидация номера карты не пропускает произвольную строку, содержащую любые нецифровые символы.', () => {
-    const validNumber = '4242 abcd 4242 4242';
-    expect(validateCVV(validNumber)).toBe(false);
+  it('Валидация номера карты не пропускает произвольную строку, содержащую любые нецифровые символы', () => {
+    const validNumber = '1233 asf2 4f42 4242';
+    expect(validateCardNumber(validNumber)).toBe(false);
   });
-
-  //3
   it('Валидация номера карты не пропускает строку с недостаточным количеством цифр', () => {
-    const validNumber = '4242 4242 4242 42';
-    expect(validateCVV(validNumber)).toBe(false);
+    const validNumber = '1233 4242 4242 42';
+    expect(validateCardNumber(validNumber)).toBe(false);
   });
-
-  //4
-  it('Валидация номера карты не пропускает строку со слишком большим количеством цифр.', () => {
-    const validNumber = '4242 4242 4242 4242 2222';
-    expect(validateCVV(validNumber)).toBe(false);
+  it('Валидация номера карты не пропускает строку со слишком большим количеством цифр', () => {
+    const validNumber = '1233 4242 4242 4242 1233';
+    expect(validateCardNumber(validNumber)).toBe(false);
   });
 });
